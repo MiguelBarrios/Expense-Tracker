e75117ee192ec57da2487acb3c4b4e4316221c89
MiguelBarrios <miguelbarriosdavila@gmail.com>
2022-04-15 20:07:20 -0500

    Stub entity, configure Hibernate

---
+++ JPAFinance/src/main/java/com/skilldistillery/finance/entities/Transaction.java
@@ -0,0 +1,42 @@
+package com.skilldistillery.finance.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Transaction {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	private double amount;
+	
+	public Transaction() {}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public double getAmount() {
+		return amount;
+	}
+
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+
+	@Override
+	public String toString() {
+		return "Transaction [id=" + id + ", amount=" + amount + "]";
+	}
+	
+	
+
+}
